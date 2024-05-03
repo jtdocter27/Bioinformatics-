8c3a8539ec9db35db2e0cfc75d9906b6123e9163
John Docter <143656896+jtdocter27@users.noreply.github.com>
2024-05-03 13:17:00 -0600

    Add files via upload

---
+++ faa_extractor.py
@@ -0,0 +1,25 @@
+import os
+import tarfile
+
+
+def tar_extraction(parent_folder):
+    # References those files that are compressed
+    extension = ".tar.gz"
+    os.chdir(parent_folder)
+    # Iterates through the protein file folder, destination where the .faa files were downloaded
+    for item in os.listdir(parent_folder):
+        filepath = os.path.join(parent_folder, item)
+        # Check for ".gz" extension, does not apply to those files that were already extracted
+        if item.endswith(extension):
+            print('File Found, Beginning Extraction')
+            print(item)
+            with tarfile.open(filepath, 'r:gz') as tar:
+            # Extract all contents of the archive
+                tar.extractall(path=parent_folder)
+            os.remove(filepath)
+
+
+
+# parent_folder = '/home/anna/Documents/EnCen_JGI_Genomes'
+parent_folder = '/home/anna/Documents/Soil_extracted_genomes'
+tar_extraction(parent_folder)
